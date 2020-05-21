119f4079efe4a139dac04b12803ba174f4a4a625
Artem Morinov <trustinthelies@gmail.com>
2020-05-21 22:37:33 +0300

    fixed newsapi request

--- src/js/modules/NewsApi.js
+++ src/js/modules/NewsApi.js
@@ -4,7 +4,7 @@ export default class NewsApi {
     const previousDate = new Date(new Date() - 604800000).toISOString();
 
     return fetch(
-      `http://newsapi.org/v2/everything?q=${input.value}&from=${previousDate}&to=${currentDate}&pageSize=100&sortBy=popularity&language=ru&apiKey=579ea2b0c42f4f78a363c7ae73ac99c1`
+      `https://cors-anywhere.herokuapp.com/http://newsapi.org/v2/everything?q=${input.value}&from=${previousDate}&to=${currentDate}&pageSize=100&sortBy=popularity&language=ru&apiKey=579ea2b0c42f4f78a363c7ae73ac99c1`
     )
       .then((res) => {
         if (res.ok) {
